544d913a4947ea7c5661b6bfe09ba20471f42115
guestx86 <guestx86@protonmail.com>
2019-10-26 12:27:06 +0200

    - It is possible to test with long list of XSS payloads(666) or with the default of 20 payloads with the new option --full
    - Added 2 new options: --timeout and --full
    - Ported from python2 to python3
    - updated geckodriver version

--- system_requirments.sh
+++ system_requirments.sh
@@ -1,8 +1,8 @@
 echo "[+] Installing python requirments"
-pip install selenium termcolor pyvirtualdisplay
+python2 -m pip install selenium termcolor pyvirtualdisplay
 sudo apt-get install xvfb
 echo "[+] Installing Geckpdriver"
-wget https://github.com/mozilla/geckodriver/releases/download/v0.20.1/geckodriver-v0.20.1-linux64.tar.gz
-mkdir /opt/geckodriver && tar zxvf geckodriver-v0.20.1-linux64.tar.gz -C /opt/geckodriver
+wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz 
+mkdir /opt/geckodriver && tar zxvf geckodriver-v0.26.0-linux64.tar.gz -C /opt/geckodriver
 echo "[+] Cleaning up .."
-rm geckodriver-v0.20.1-linux64.tar.gz
+rm geckodriver-v0.26.0-linux64.tar.gz
