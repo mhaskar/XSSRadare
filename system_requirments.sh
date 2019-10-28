1c90e01a5fa073810887bcb66e8795e008432747
Askar <m.askar@isecur1ty.org>
2019-10-28 19:35:00 +0200

    pip3 installation

--- system_requirments.sh
+++ system_requirments.sh
@@ -1,6 +1,8 @@
+echo "[+] Installing pip3"
+sudo apt install python3-pip
 echo "[+] Installing python requirments"
-python2 -m pip install selenium termcolor pyvirtualdisplay
-sudo apt-get install xvfb
+pip3 install selenium termcolor pyvirtualdisplay
+sudo apt install xvfb
 echo "[+] Installing Geckpdriver"
 wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz 
 mkdir /opt/geckodriver && tar zxvf geckodriver-v0.26.0-linux64.tar.gz -C /opt/geckodriver
